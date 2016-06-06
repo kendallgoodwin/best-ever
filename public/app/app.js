2465e8f97315c0934339a881a5a0e12d8a744757
kendallgoodwin <kendall@kendallgoodwin.com>
2016-06-06 12:19:54 -0700

    routing & states

--- public/app/app.js
+++ public/app/app.js
@@ -4,8 +4,13 @@ angular.module('BestEverApp', ['BestEverCtrls', 'ui.router'])
   $urlRouterProvider.otherwise('/404');
 
   $stateProvider
-  .state('signup', {
+  .state('landing', {
     url: '/',
+    templateUrl: 'views/all_entries.html',
+    controller: 'EntriesCtrl'
+  })
+  .state('signup', {
+    url: '/signup',
     templateUrl: 'views/signup.html',
     controller: 'SignUpCtrl'
   })
@@ -13,6 +18,26 @@ angular.module('BestEverApp', ['BestEverCtrls', 'ui.router'])
     url: '/login',
     templateUrl: 'views/login.html',
     controller: 'LoginCtrl'
+  })
+  .state('dashboard', {
+    url: '/dashboard',
+    templateUrl: 'views/dashboard.html',
+    controller: 'DashboardCtrl'
+  })
+  .state('profile', {
+    url: '/profile',
+    templateUrl: 'views/profile.html',
+    controller: 'ProfileCtrl'
+  })
+  .state('newEntry', {
+    url: '/new_entry',
+    templateUrl: 'views/new_entry.html',
+    controller: 'NewCtrl'
+  })
+  .state('viewEntry', {
+    url: '/view_entry',
+    templateUrl: 'views/view_entry.html',
+    controller: 'ViewCtrl'
   })
   .state('404', {
     url: '/404',
--- public/app/controllers.js
+++ public/app/controllers.js
@@ -1,17 +1,35 @@
 angular.module('BestEverCtrls', ['BestEverServices'])
 
-.controller('SignUpCtrl', ['$scope', '$stateParams', function($scope, $stateParams) {
-  $scope.film = {};
+.controller('EntriesCtrl', ['$scope', '$stateParams', function($scope, $stateParams) {
+	$state.go('landing');
+}])
 
-  User.get({id: $stateParams.id}, function success(data) {
-    $scope.film = data;
-  }, function error(data) {
-    console.log(data);
-  });
+.controller('SignUpCtrl', ['$scope', '$stateParams', function($scope, $stateParams) {
+  // User.get({id: $stateParams.id}, function success(data) {
+  //   $scope.film = data;
+  // }, function error(data) {
+  //   console.log(data);
+  // });
 
   $state.go('signup');
 }])
-.controller('LoginCtrl', ['$scope', '$stateParams', function($scope, $stateParams) {
 
+.controller('LoginCtrl', ['$scope', '$stateParams', function($scope, $stateParams) {
 	$state.go('login');
-	}])
+}])
+
+.controller('DashboardCtrl', ['$scope', '$stateParams', function($scope, $stateParams) {
+	$state.go('dashboard');
+}])
+
+.controller('ProfileCtrl', ['$scope', '$stateParams', function($scope, $stateParams) {
+	$state.go('profile');
+}])
+
+.controller('NewCtrl', ['$scope', '$stateParams', function($scope, $stateParams) {
+	$state.go('newEntry');
+}])
+
+.controller('ViewCtrl', ['$scope', '$stateParams', function($scope, $stateParams) {
+	$state.go('viewEntry');
+}])
